85294d28e09de0325935c7fb59d3def3860d0b2d
SuperUser <yeungbluecat123@gmail.com>
2017-02-15 22:43:09 +0800

    First Release

---
+++ ConsoleApplication2/Class1.vb
@@ -0,0 +1,216 @@
+﻿
+Imports System.Collections.Generic
+Imports System.IO
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Text.RegularExpressions
+
+Namespace eMZi.Gaming.Minecraft
+    ''' <summary>
+    ''' Represents retrieved Minecraft Server information
+    ''' </summary>
+    Public NotInheritable Class MinecraftServerInfo
+        ''' <summary>
+        ''' Gets the server's MOTD
+        ''' </summary>
+        Public Property ServerMotd() As String
+            Get
+                Return m_ServerMotd
+            End Get
+            Private Set(value As String)
+                m_ServerMotd = Value
+            End Set
+        End Property
+        Private m_ServerMotd As String
+
+        ''' <summary>
+        ''' Gets the server's MOTD converted into HTML
+        ''' </summary>
+        Public ReadOnly Property ServerMotdHtml() As String
+            Get
+                Return Me.MotdHtml()
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Gets the server's max player count
+        ''' </summary>
+        Public Property MaxPlayerCount() As Integer
+            Get
+                Return m_MaxPlayerCount
+            End Get
+            Private Set(value As Integer)
+                m_MaxPlayerCount = Value
+            End Set
+        End Property
+        Private m_MaxPlayerCount As Integer
+
+        ''' <summary>
+        ''' Gets the server's current player count
+        ''' </summary>
+        Public Property CurrentPlayerCount() As Integer
+            Get
+                Return m_CurrentPlayerCount
+            End Get
+            Private Set(value As Integer)
+                m_CurrentPlayerCount = Value
+            End Set
+        End Property
+        Private m_CurrentPlayerCount As Integer
+
+        ''' <summary>
+        ''' Gets the server's Minecraft version
+        ''' </summary>
+        Public Property MinecraftVersion() As Version
+            Get
+                Return m_MinecraftVersion
+            End Get
+            Private Set(value As Version)
+                m_MinecraftVersion = Value
+            End Set
+        End Property
+        Private m_MinecraftVersion As Version
+
+        ''' <summary>
+        ''' Gets HTML colors associated with specific formatting codes
+        ''' </summary>
+        Private Shared ReadOnly Property MinecraftColors() As Dictionary(Of Char, String)
+            Get
+                Return New Dictionary(Of Char, String)() From { _
+                    {"0"c, "#000000"}, _
+                    {"1"c, "#0000AA"}, _
+                    {"2"c, "#00AA00"}, _
+                    {"3"c, "#00AAAA"}, _
+                    {"4"c, "#AA0000"}, _
+                    {"5"c, "#AA00AA"}, _
+                    {"6"c, "#FFAA00"}, _
+                    {"7"c, "#AAAAAA"}, _
+                    {"8"c, "#555555"}, _
+                    {"9"c, "#5555FF"}, _
+                    {"a"c, "#55FF55"}, _
+                    {"b"c, "#55FFFF"}, _
+                    {"c"c, "#FF5555"}, _
+                    {"d"c, "#FF55FF"}, _
+                    {"e"c, "#FFFF55"}, _
+                    {"f"c, "#FFFFFF"} _
+                }
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Gets HTML styles associated with specific formatting codes
+        ''' </summary>
+        Private Shared ReadOnly Property MinecraftStyles() As Dictionary(Of Char, String)
+            Get
+                Return New Dictionary(Of Char, String)() From { _
+                    {"k"c, "none;font-weight:normal;font-style:normal"}, _
+                    {"m"c, "line-through;font-weight:normal;font-style:normal"}, _
+                    {"l"c, "none;font-weight:900;font-style:normal"}, _
+                    {"n"c, "underline;font-weight:normal;font-style:normal;"}, _
+                    {"o"c, "none;font-weight:normal;font-style:italic;"}, _
+                    {"r"c, "none;font-weight:normal;font-style:normal;color:#FFFFFF;"} _
+                }
+            End Get
+        End Property
+
+        ''' <summary>
+        ''' Creates a new instance of <see cref="MinecraftServerInfo"/> with specified values
+        ''' </summary>
+        ''' <param name="motd">Server's MOTD</param>
+        ''' <param name="maxplayers">Server's max player count</param>
+        ''' <param name="playercount">Server's current player count</param>
+        ''' <param name="version">Server's Minecraft version</param>
+        Private Sub New(motd As String, maxplayers As Integer, playercount As Integer, mcversion As Version)
+            Me.ServerMotd = motd
+            Me.MaxPlayerCount = maxplayers
+            Me.CurrentPlayerCount = playercount
+            Me.MinecraftVersion = mcversion
+        End Sub
+
+        ''' <summary>
+        ''' Gets the server's MOTD formatted as HTML
+        ''' </summary>
+        ''' <returns>HTML-formatted MOTD</returns>
+        Private Function MotdHtml() As String
+            Dim regex As New Regex("§([k-oK-O])(.*?)(§[0-9a-fA-Fk-oK-OrR]|$)")
+            Dim s As String = Me.ServerMotd
+            While regex.IsMatch(s)
+                s = regex.Replace(s, Function(m)
+                                                     Dim ast As String = "text-decoration:" + MinecraftStyles(m.Groups(1).Value(0))
+                                                     Dim html As String = (Convert.ToString("<span style=""") & ast) + """>" + m.Groups(2).Value + "</span>" + m.Groups(3).Value
+                                                     Return html
+
+                                                 End Function)
+            End While
+            regex = New Regex("§([0-9a-fA-F])(.*?)(§[0-9a-fA-FrR]|$)")
+            While regex.IsMatch(s)
+                s = regex.Replace(s, Function(m)
+                                                     Dim ast As String = "color:" + MinecraftColors(m.Groups(1).Value(0))
+                                                     Dim html As String = (Convert.ToString("<span style=""") & ast) + """>" + m.Groups(2).Value + "</span>" + m.Groups(3).Value
+                                                     Return html
+
+                                                 End Function)
+            End While
+            Return s
+        End Function
+
+        ''' <summary>
+        ''' Gets the information from specified server
+        ''' </summary>
+        ''' <param name="endpoint">IP and Port of the server to get information from</param>
+        ''' <returns>A <see cref="MinecraftServerInformation"/> instance with retrieved data</returns>
+        ''' <exception cref="System.Exception">Upon failure, throws exception with descriptive information and InnerException with details</exception>
+        Public Shared Function GetServerInformation(endpoint As IPEndPoint) As MinecraftServerInfo
+            If endpoint Is Nothing Then
+                Throw New ArgumentNullException("endpoint")
+            End If
+            Try
+                Dim packetdat As String() = Nothing
+                Using client As New TcpClient()
+                    client.Connect(endpoint)
+                    Using ns As NetworkStream = client.GetStream()
+                        ns.Write(New Byte() {&HFE, &H1}, 0, 2)
+                        Dim buff As Byte() = New Byte(2047) {}
+                        Dim br As Integer = ns.Read(buff, 0, buff.Length)
+                        If buff(0) <> &HFF Then
+                            Throw New InvalidDataException("Received invalid packet")
+                        End If
+                        Dim packet As String = Encoding.BigEndianUnicode.GetString(buff, 3, br - 3)
+                        If Not packet.StartsWith("§") Then
+                            Throw New InvalidDataException("Received invalid data")
+                        End If
+                        packetdat = packet.Split(ControlChars.NullChar)
+                        ns.Close()
+                    End Using
+                    client.Close()
+                End Using
+                Return New MinecraftServerInfo(packetdat(3), Integer.Parse(packetdat(5)), Integer.Parse(packetdat(4)), Version.Parse(packetdat(2)))
+            Catch ex As SocketException
+                Throw New Exception("There was a connection problem, look into InnerException for details", ex)
+            Catch ex As InvalidDataException
+                Throw New Exception("The data received was invalid, look into InnerException for details", ex)
+            Catch ex As Exception
+                Throw New Exception("There was a problem, look into InnerException for details", ex)
+            End Try
+        End Function
+
+        ''' <summary>
+        ''' Gets the information from specified server
+        ''' </summary>
+        ''' <param name="ip">IP of the server to get info from</param>
+        ''' <param name="port">Port of the server to get info from</param>
+        ''' <returns>A <see cref="MinecraftServerInformation"/> instance with retrieved data</returns>
+        ''' <exception cref="System.Exception">Upon failure, throws exception with descriptive information and InnerException with details</exception>
+        Public Shared Function GetServerInformation(ip As IPAddress, port As Integer) As MinecraftServerInfo
+            Return GetServerInformation(New IPEndPoint(ip, port))
+        End Function
+    End Class
+End Namespace
+
+'=======================================================
+'Service provided by Telerik (www.telerik.com)
+'Conversion powered by NRefactory.
+'Twitter: @telerik
+'Facebook: facebook.com/telerik
+'=======================================================
---
+++ ConsoleApplication2/Module1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.Data
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Module Module1
+
+    Sub Main()
+
+
+
+        Dim soc As New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+
+        soc.Bind(New IPEndPoint(IPAddress.Any, 8585))
+        soc.Listen(10)
+        Console.WriteLine("Web Server Started ...")
+
+        While True
+            Try
+
+
+
+                Dim client As Socket = soc.Accept
+                Dim sb As New System.Text.StringBuilder
+                Dim html As String = File.ReadAllText("index.html")
+                Dim htmlHeader As String = _
+                "HTTP/1.0 200 OK" & ControlChars.CrLf & _
+                "Server: WebServer 1.0" & ControlChars.CrLf & _
+                "Content-Length: " & html.Length & ControlChars.CrLf & _
+                "Content-Type: text/html" & _
+                ControlChars.CrLf & ControlChars.CrLf
+
+                Dim headerByte() As Byte = Encoding.ASCII.GetBytes(htmlHeader)
+                client.Send(headerByte, headerByte.Length, SocketFlags.None)
+
+                client.Receive(headerByte, headerByte.Length, SocketFlags.None)
+                'Console.WriteLine(Encoding.ASCII.GetString(headerByte).Split(" ")(1))
+
+                Dim txt As String = Encoding.ASCII.GetString(headerByte).Split(" ")(1)
+                
+                Dim ip As String = Nothing
+                Dim iph As IPHostEntry = Dns.GetHostEntry(txt.Replace("/", ""))
+                Console.WriteLine(txt.Replace("/", ""))
+                For Each ip1 As IPAddress In iph.AddressList
+                    ip = ip1.ToString()
+                    ' TODO: might not be correct. Was : Exit For
+                    Exit For
+                Next
+                Dim a As eMZi.Gaming.Minecraft.MinecraftServerInfo = Nothing
+                Dim c As IPAddress = Nothing
+                Dim Err As Boolean = False
+                c = IPAddress.Parse(ip)
+                Dim b As New IPEndPoint(c, 25565)
+
+                Try
+                    a = eMZi.Gaming.Minecraft.MinecraftServerInfo.GetServerInformation(b)
+                Catch ex As Exception
+                    html = html.Replace("{0}", "").Replace("{1}", "").Replace("{2}", "").Replace("{3}", "")
+                    Dim htmlByte1() As Byte = Encoding.ASCII.GetBytes(html)
+
+                    client.Send(htmlByte1, 0, htmlByte1.Length, SocketFlags.None)
+                    Err = True
+                End Try
+
+                If Err = False Then
+                    a = eMZi.Gaming.Minecraft.MinecraftServerInfo.GetServerInformation(b)
+                    html = html.Replace("{0}", txt.Replace("/", "")).Replace("{1}", a.ServerMotd).Replace("{2}", a.CurrentPlayerCount & "/" & a.MaxPlayerCount).Replace("{3}", a.MinecraftVersion.ToString)
+                    Dim htmlByte() As Byte = Encoding.ASCII.GetBytes(html)
+
+                    client.Send(htmlByte, 0, htmlByte.Length, SocketFlags.None)
+
+                End If
+
+
+
+
+
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+            End Try
+
+        End While
+
+
+
+
+
+    End Sub
+
+
+
+
+End Module
